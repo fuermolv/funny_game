8848326162a4e60c114ec716c633521f9db9610b
Unicorn2018 <460932974@qq.com>
2017-11-02 10:16:06 +0800

    添加工程文件
    添加工程文件

---
+++ 1614/1614/Assets/GameScript/adimatorController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class adimatorController : MonoBehaviour
+{
+
+    private Animator solider;
+    public move a;
+
+    // Use this for initialization
+    void Start()
+    {
+        a = this.gameObject.GetComponent<move>();
+        solider = this.gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (a.MoveAllow == 1)
+            solider.SetBool("move", true);
+        if (a.MoveAllow != 1)
+            solider.SetBool("move", false);
+    }
+}
